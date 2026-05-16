304785bbdf39291b9bed851ff91553c281cd63da
weedygmd <canix-weedy@proton.me>
2026-05-17 00:49:42 +0300

    added live input

--- usr/bin/factor.c
+++ usr/bin/factor.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <cervus_util.h>
 
 int is_number(const char *s) {
@@ -17,9 +18,42 @@ int is_number(const char *s) {
 }
 
 int main(int argc, char **argv) {
-	if (argc != 2) {
+/*	if (argc != 2) {
 		fprintf(stderr, C_RED "factor: variable not specified\n" C_RESET);
         return 1;
+	}*/
+
+	if (argc != 2) {
+		printf(C_CYAN "Cervus factor, write integer and press enter. exit or ctrl+c to quit\n" C_RESET);
+		while (1) {
+			char num[128];
+			fgets(num, sizeof(num), stdin);
+			num[strcspn(num, "\n")] = '\0';
+
+			if (strcmp(num, "exit") == 0) {
+				printf("exiting..");
+				return 1;
+			}
+		
+			if (!is_number(num)) {
+				fprintf(stderr, C_RED "factor: %s isn't number\n" C_RESET, argv[1]);
+			    return 1;
+			}
+			
+			int n = atoi(num);
+			int d = 2;
+			
+			printf("%d: ", n);
+			do {
+				if (n % d == 0) {
+					printf("%d ", d);
+					n = n / d;
+				} else {
+					d++;
+				}
+			} while (n > 1);
+			printf("\n");
+		}
 	}
 
 	if (!is_number(argv[1])) {
